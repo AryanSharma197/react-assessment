e96d5fca5a38dd4aa1d570cac35ea7cae812539f
Aryan Sharma <95480865+AryanSharma197@users.noreply.github.com>
2024-07-13 17:52:47 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React from 'react';
+import BasketballPlayerCard from './components/BasketballPlayerCard';
+import players from './data/playersData';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {players.map((player) => (
+        <BasketballPlayerCard
+          key={player.name}
+          name={player.name}
+          image={player.image}
+          position={player.position}
+          stats={player.stats}
+        />
+      ))}
     </div>
   );
 }
---
+++ src/data/playersData.js
@@ -0,0 +1,39 @@
+import img1 from '../images/lebronJames.jpg'
+import img2 from '../images/stephernCurry.jpg'
+import img3 from '../images/kevinDurant.jpg'
+
+const players = [
+    {
+      name: 'LeBron James',
+      image: img1,
+      position: 'Forward',
+      stats: {
+        pointsPerGame: 25.4,
+        assistsPerGame: 7.1,
+        reboundsPerGame: 10.5,
+      },
+    },
+    {
+      name: 'Stephen Curry',
+      image: img2,
+      position: 'Guard',
+      stats: {
+        pointsPerGame: 27.3,
+        assistsPerGame: 6.2,
+        reboundsPerGame: 5.3,
+      },
+    },
+    {
+      name: 'Kevin Durant',
+      image: img3,
+      position: 'Forward',
+      stats: {
+        pointsPerGame: 26.9,
+        assistsPerGame: 5.6,
+        reboundsPerGame: 7.1,
+      },
+    },
+  ];
+  
+  export default players;
+  
